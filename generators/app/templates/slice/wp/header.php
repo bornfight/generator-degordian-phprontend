a311c48a8fd2226bd0bd1d7190ea9852528e6da1
renatoruk <renato.ruk@gmail.com>
2018-02-20 11:01:19 +0100

    added default favicon paths inside head

--- generators/app/templates/slice/wp/header.php
+++ generators/app/templates/slice/wp/header.php
@@ -3,13 +3,40 @@
 ?>
 <!DOCTYPE html>
 <head>
+    <?php
+    //IMPORTANT NOTE! Order of tags should be as shown in this file
+    // 1. META TAGS
+    ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="author" content="Degordian (http://www.degordian.com)">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php
+    // 2. FAVICONS
+    ?>
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= buStatic('favicon/apple-touch-icon.png'); ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= buStatic('favicon/favicon-32x32.png'); ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= buStatic('favicon/favicon-16x16.png'); ?>">
+    <link rel="manifest" href="<?= buStatic('favicon/manifest.json'); ?>">
+    <link rel="mask-icon" href="<?= buStatic('favicon/safari-pinned-tab.svg'); ?>" color="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="msapplication-navbutton-color" content="#ffffff">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff">
+    <?php
+    // 3. TITLE
+    ?>
     <title><?= ucfirst(PROJECT_NAME) ?></title>
-    <!-- CSS -->
+    <?php
+    // 4. CRITICAL CSS
+    // 5. ALL CSS
+    ?>
+    <style id="critical-css">
+
+    </style>
     <link rel="stylesheet" href="<?= buStatic('dist/style.css') ?>">
+    <?php
+    // 6. ALL SCRIPTS
+    ?>
     <script defer src="<?= buStatic('dist/vendor.js') ?>"></script>
     <script defer src="<?= buStatic('dist/bundle.js') ?>"></script>
     <?php wp_head(); ?>
--- generators/app/templates/slice/yii/layout/_head.php
+++ generators/app/templates/slice/yii/layout/_head.php
@@ -1,12 +1,39 @@
 <!-- HTML BEGIN -->
 <head>
+    <?php
+    //IMPORTANT NOTE! Order of tags should be as shown in this file
+    // 1. META TAGS
+    ?>
     <meta charset="utf-8">
-    <title><?= ucfirst(PROJECT_NAME) ?></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="author" content="Degordian (http://www.degordian.com)">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?php
+    // 2. FAVICONS
+    ?>
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= bu('static/favicon/apple-touch-icon.png'); ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= bu('static/favicon/favicon-32x32.png'); ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= bu('static/favicon/favicon-16x16.png'); ?>">
+    <link rel="manifest" href="<?= bu('static/favicon/manifest.json'); ?>">
+    <link rel="mask-icon" href="<?= bu('static/favicon/safari-pinned-tab.svg'); ?>" color="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="msapplication-navbutton-color" content="#ffffff">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff">
+    <?php
+    // 3. TITLE
+    ?>
+    <title><?= ucfirst(PROJECT_NAME) ?></title>
+    <?php
+    // 4. CRITICAL CSS
+    // 5. ALL CSS
+    ?>
+    <style id="critical-css">
 
-    <link rel="stylesheet" href="<?=bu('static/dist/style.css')?>">
-    <script defer src="<?=bu('static/dist/vendor.js')?>"></script>
-    <script defer src="<?=bu('static/dist/bundle.js')?>"></script>
+    </style>
+    <link rel="stylesheet" href="<?= bu('static/dist/style.css') ?>">
+    <?php
+    // 6. ALL SCRIPTS
+    ?>
+    <script defer src="<?= bu('static/dist/vendor.js') ?>"></script>
+    <script defer src="<?= bu('static/dist/bundle.js') ?>"></script>
 </head>
